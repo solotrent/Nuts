c801a22ed9d1a26a4877e038f8cc6173ad312cd4
solotrent <152901482+solotrent@users.noreply.github.com>
2025-09-07 18:38:27 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+}
+
+module.exports = nextConfig
